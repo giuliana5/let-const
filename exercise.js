e0dc29bdbb8dbea3870b84039e672bd71f9f6ddc
Giuliana <giuliana.torres05@gmail.com>
2022-07-02 14:13:55 -0600

    Refactored ES5 code into ES2015.

---
+++ exercise.js
@@ -0,0 +1,28 @@
+//ES5 Global Constants
+var PI = 3.14;
+PI = 42;
+
+//ES2015 Global Constants
+const PI = 3.14;
+/*
+Difference between var and let:
+
+- var can be redeclared and is contained in a function scope
+- var variabled are hoisted
+- git let has a block scope and cannot be redeclared
+
+Difference between var and const:
+
+- var can be redeclared and reassigned, along with having a function scope
+- var variabled are hoisted
+- const has a block scope and cannot be redeclared or reassigned.
+
+Different between let and const:
+
+- let can be reassigned
+- const cannot be reassigned
+
+Hoisting:
+
+-Hoisting is when the variable declarations are lifted above other code within that scope.
+*/
